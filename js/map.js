07c92d63e8ff19f7a39e25c87a643de5b9915a3a
valium85 <backtothecaves85@gmail.com>
2020-03-17 17:54:18 +0300

    Теперь перетаскивание работает правильно

--- js/map.js
+++ js/map.js
@@ -1,102 +1,117 @@
 'use strict';
 
-// Преобразование в активное состояние
+// Преобразование в активное состояние и перетаскивание метки
 
+(function () {
+  var ENTER_KEY = 'Enter';
+  var CLICK = 1;
+  var adForm = document.querySelector('.ad-form');
+  var adFormItems = adForm.children;
+  var mapFilters = document.querySelector('.map__filters');
+  var mapFiltersItems = mapFilters.children;
+  var mapPinMain = document.querySelector('.map__pin--main');
+  var formAddress = adForm.querySelector('#address');
+  var pinLeftBorder = 0;
+  var pinRightBorder = 1200;
+  var pinTopBorder = 130;
+  var pinBottomBorder = 630;
+  var pinXOffset = 65 / 2;
+  var pinYOffset = 65 + 22;
 
-var ENTER_KEY = 'Enter';
-var CLICK = 1;
-var adForm = document.querySelector('.ad-form');
-var adFormItems = adForm.children;
-var mapFilters = document.querySelector('.map__filters');
-var mapFiltersItems = mapFilters.children;
-var mapPinMain = document.querySelector('.map__pin--main');
-var formAddress = adForm.querySelector('#address');
-var pinXOffset = 65 / 2;
-var pinYOffset = 65 + 22;
+  var getPinX = function (pinButton) {
+    return parseInt(pinButton.style.left, 10) + Math.round(pinXOffset);
+  };
 
-var getPinX = function (pinButton) {
-  return parseInt(pinButton.style.left, 10) + Math.round(pinXOffset);
-};
+  var getPinY = function (pinButton) {
+    return parseInt(pinButton.style.top, 10) + Math.round(pinYOffset);
+  };
 
-var getPinY = function (pinButton) {
-  return parseInt(pinButton.style.top, 10) + Math.round(pinYOffset);
-};
+  var getPinAddress = function (pinButton) {
 
-var getPinAddress = function (pinButton) {
+    return getPinX(pinButton) + ', ' + getPinY(pinButton);
+  };
 
-  return getPinX(pinButton) + ', ' + getPinY(pinButton);
-};
+  var makeFormsActive = function () {
+    for (var i = 0; i < adFormItems.length; i++) {
+      adFormItems[i].removeAttribute('disabled');
+    }
 
-var makeFormsActive = function () {
-  for (var i = 0; i < adFormItems.length; i++) {
-    adFormItems[i].removeAttribute('disabled');
-  }
+    for (var j = 0; j < mapFiltersItems.length; j++) {
+      mapFiltersItems[j].removeAttribute('disabled');
+    }
 
-  for (var j = 0; j < mapFiltersItems.length; j++) {
-    mapFiltersItems[j].removeAttribute('disabled');
-  }
+    adForm.classList.remove('ad-form--disabled');
+    formAddress.value = getPinAddress(mapPinMain);
+  };
 
-  adForm.classList.remove('ad-form--disabled');
-  formAddress.value = getPinAddress(mapPinMain);
-};
+  var makeOtherActive = function () {
+    document.querySelector('.map').classList.remove('map--faded');
+    window.pins.renderAll(window.data.mockAds);
+    window.card.addCardTemplate();
+  };
 
-var makeOtherActive = function () {
-  document.querySelector('.map').classList.remove('map--faded');
-  window.pins.renderAll(window.data.mockAds);
-  window.card.addCardTemplate();
-};
+  var onPinMouseDown = function (pin) {
+    pin.addEventListener('mousedown', function (evt) {
 
-var onPinMouseDown = function (pin) {
-  pin.addEventListener('mousedown', function (evt) {
+      if (evt.which === CLICK) {
+        evt.preventDefault();
 
-    if (evt.which === CLICK) {
-      evt.preventDefault();
-
-      var startCoords = {
-        x: evt.clientX,
-        y: evt.clientY
-      };
-
-      var onMouseMove = function (moveEvt) {
-        moveEvt.preventDefault();
-
-        var shift = {
-          x: startCoords.x - moveEvt.clientX,
-          y: startCoords.y - moveEvt.clientY
+        var startCoords = {
+          x: evt.clientX,
+          y: evt.clientY
         };
 
-        startCoords = {
-          x: moveEvt.clientX,
-          y: moveEvt.clientY
+        var onMouseMove = function (moveEvt) {
+          moveEvt.preventDefault();
+
+          var shift = {
+            x: startCoords.x - moveEvt.clientX,
+            y: startCoords.y - moveEvt.clientY
+          };
+
+          startCoords = {
+            x: moveEvt.clientX,
+            y: moveEvt.clientY
+          };
+
+          pin.style.top = (pin.offsetTop - shift.y) + 'px';
+          pin.style.left = (pin.offsetLeft - shift.x) + 'px';
+
+          if (getPinX(pin) < pinLeftBorder) {
+            pin.style.left = (pinLeftBorder - pinXOffset) + 'px';
+          } else if (getPinX(pin) > pinRightBorder) {
+            pin.style.left = (pinRightBorder - pinXOffset) + 'px';
+          } else if (getPinY(pin) < pinTopBorder) {
+            pin.style.top = (pinTopBorder - pinYOffset) + 'px';
+          } else if (getPinY(pin) > pinBottomBorder) {
+            pin.style.top = (pinBottomBorder - pinYOffset) + 'px';
+          }
+
+          formAddress.value = getPinAddress(pin);
         };
 
-        pin.style.top = (pin.offsetTop - shift.y) + 'px';
-        pin.style.left = (pin.offsetLeft - shift.x) + 'px';
-        formAddress.value = getPinAddress(pin);
+        var onMouseUp = function (upEvt) {
+          upEvt.preventDefault();
 
-      };
+          document.removeEventListener('mousemove', onMouseMove);
+          document.removeEventListener('mouseup', onMouseUp);
+          makeFormsActive();
+          makeOtherActive();
+        };
 
-      var onMouseUp = function (upEvt) {
-        upEvt.preventDefault();
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp);
+      }
+    });
+  };
 
-        document.removeEventListener('mousemove', onMouseMove);
-        document.removeEventListener('mouseup', onMouseUp);
-        makeFormsActive();
-        makeOtherActive();
-      };
+  onPinMouseDown(mapPinMain);
 
-      document.addEventListener('mousemove', onMouseMove);
-      document.addEventListener('mouseup', onMouseUp);
+  mapPinMain.addEventListener('keydown', function (evt) {
+    if (evt.key === ENTER_KEY) {
+      makeFormsActive();
+      makeOtherActive();
     }
   });
-}
 
-onPinMouseDown(mapPinMain);
-
-mapPinMain.addEventListener('keydown', function (evt) {
-  if (evt.key === ENTER_KEY) {
-    makeFormsActive();
-    makeOtherActive();
-  }
-});
-
+})();
